e0eb214cc6a77971a133f40d6e99313740eac12b
Xiark99 <221327809+Xiark99@users.noreply.github.com>
2026-09-19 10:26:23 +0900

    Bump version code to 77

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.vivi.vivimusic"
         minSdk = 26
         targetSdk = 37
-        versionCode = 76
+        versionCode = 77
         val betaVersionName = project.findProperty("betaVersionName") as String?
         versionName = betaVersionName ?: "6.0.7"
 
